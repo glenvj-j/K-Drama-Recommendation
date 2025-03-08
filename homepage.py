82fb69263a63d937b7883fbe3aba9eee44dd361f
Glen Valencius <glen.valencius@gmail.com>
2025-03-08 20:51:24 +0700

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 import time
+import warnings
+
+warnings.simplefilter(action='ignore')
 
 st.set_page_config(
     page_title="K-Drama Recomendation",
